5ff68a5d19f65d0a5bc5ac2c8cedf8676c216653
ilorlando10 <orlandolife@hotmail.com>
2022-03-19 16:03:36 +0000

    Finished Project

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let icons = ["&#9824", "&#9827", "&#9829", "&#9830"];
+  let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+
+  let randomIcon = icons[Math.floor(Math.random() * icons.length)];
+  let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+
+  if (randomIcon == "&#9830" || randomIcon == "&#9829") {
+    document.querySelector(".top").style.color = "red";
+    document.querySelector(".middle").style.color = "red";
+    document.querySelector(".bottom").style.color = "red";
+  }
+
+  document.querySelector(".top").innerHTML = randomIcon;
+  document.querySelector(".bottom").innerHTML = randomIcon;
+  document.querySelector(".middle").innerHTML = randomNumber;
 };
